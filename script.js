3a6445c21e2b5390622adf45eb54e869b888e572
douglas <douglasrc1912@gmail.com>
2024-01-15 08:53:06 -0300

    Java Script

---
+++ script.js
@@ -0,0 +1,77 @@
+/*
+por tag: getgetElementByName()
+por id: getgetElementById()
+por Nome: getgetElementsByName()
+por Classe: getgetElementsByClassName()
+por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+let nomeOk = 'false'
+let emailOk = 'false'
+let assuntoOk = 'false'
+let mapa = document.querySelector('#mapa')
+
+nome.style.width = '100%'
+nome.style.width = '100%'
+
+function validaNome() {
+    let txtNome = document.querySelector("#txtNome")
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome válido'
+        txtNome.style.color = 'geen'
+        nomeOk = true
+    }
+}
+
+function validaEmail() {
+    let txtEmail = document.querySelector("#txtEmail")
+
+    if (email.value.indexof('@') == -1 || email.value.indexof('.') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'geen'
+        emailOk = true
+    }
+
+    function validaAssunto() {
+        let txtAssunto = document.querySelector("#txtAssunto")
+
+        if (assunto.value.length >= 100) {
+            txtAssunto.innerHTML = 'Texto muito grande, digite no máximo 100 caracteres'
+            txtAssunto.style.color = 'red'
+            txtAssunto.style.display = 'block'
+        } else {
+            txtAssunto.style.display = 'none'
+            txtAssunto.style.color = 'green'
+            assuntoOk = true
+
+        }
+    }
+
+    function enviar() {
+        if (nomeOk == true && emailOk == true && assuntoOk == true) {
+            alert("Formulário enciado com sucesso!")
+        } else {
+            alert("Preencha o formulário corretamente antes de enviar...")
+        }
+    }
+
+    function mapaZoom() {
+        mapa.style.width = '800px'
+        mapa.style.height = '600px'
+    }
+
+    function mapaNormal() {
+        mapa.style.width = '400px'
+        mapa.style.height = '250px'
+    }
+
+}
